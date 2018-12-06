1fb44b0e05c53160f8f8702a68d8ed38c64677c2
six.nonacosa <pkwenda@163.com>
2018-12-06 23:31:26 +0800

    扩展类型

--- src/tree/entry/entry.go
+++ src/tree/entry/entry.go
@@ -5,6 +5,26 @@ import (
 	"tree"
 )
 
+/**
+ 如何扩展已有类型
+ */
+type MyNode struct {
+	node *tree.Node
+}
+
+func (myNode *MyNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+
+	//cannot call pointer method on MyNode literal 不能这样直接用包装过得调用方法，要用变量
+	//MyNode{myNode.node.Left}.postOrder()
+	left := MyNode{myNode.node.Left}
+	right := MyNode{myNode.node.Right}
+	left.postOrder()
+	right.postOrder()
+	myNode.node.Print()
+}
 
 
 func main() {
@@ -35,5 +55,11 @@ func main() {
 
 	// 便利树
 	root.Traverse()
+
+	// cannot call pointer method on MyNode literal
+	//MyNode{&root}.postOrder()
+	myNote := MyNode{&root}
+	myNote.postOrder()
+
 
 }
