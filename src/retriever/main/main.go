950e172686391e6b83d62e86d583e284b9e8c169
six.nonacosa <pkwenda@163.com>
2018-12-07 01:16:51 +0800

    go interface type

--- src/retriever/main/main.go
+++ src/retriever/main/main.go
@@ -12,7 +12,32 @@ func download(r retriever.Retriever) string {
 
 func main() {
 	var r  retriever.Retriever
-	r = httpRetriever.HttpRetriever{"apple"}
+	r = httpRetriever.HttpRetriever{"name"}
 
 	fmt.Println(download(r))
+	fmt.Printf("%T , %v\n",r,r)
+
+	/**
+	 如何知道接口的类型 type ？
+	 */
+	//1 - switch type： 已知或者未知的对象数据类型均可，r.(type)必须配合switch来使用，不能单独执行此语句。
+	switch v := r.(type) {
+	case httpRetriever.HttpRetriever:
+		fmt.Println(v.Name)
+	}
+
+	// 2 - type assertion (断言）类似 java 中的<泛型> 调用其他实现者的方法会直接报错
+
+	//httpRetriever := r.(httpRetriever.HttpRetriever)
+	//fmt.Println(httpRetriever.Name)
+
+	if httpRetriever,ok := r.(httpRetriever.HttpRetriever); ok {
+		fmt.Println(httpRetriever.Name)
+	} else {
+		fmt.Println("not a httpRetriever ")
+	}
+
+
+
+
 }
